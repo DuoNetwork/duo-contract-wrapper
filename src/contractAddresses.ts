9eb57e8826b8baf4d908448a30d9c4fabca9b193
WYANG008 <weitao.yang@finbook.co>
2019-03-04 11:50:36 +0800

    update

--- src/contractAddresses.ts
+++ src/contractAddresses.ts
@@ -16,21 +16,7 @@ export const kovan: IContractAddresses = {
 					code: 'bETH',
 					address: '0x8A3bEca74E0E737460bDE45a09594A8D7D8c9886'
 				}
-			},
-			[CST.TENOR_M19]: {
-				custodian: {
-					code: 'BEETHOVEN-M19',
-					address: '0x1e20057a6Ddc009b275926d7d54cCA19625c974E'
-				},
-				aToken: {
-					code: 'aETH-M19',
-					address: '0xB608f08d94662453ac8fce91f5956CCd00031449'
-				},
-				bToken: {
-					code: 'bETH-M19',
-					address: '0xe6c92061a8890FFC429485c36fa76853ddC4f07D'
-				}
-			},
+			}
 		},
 		[CST.MOZART]: {
 			[CST.TENOR_PPT]: {
@@ -46,21 +32,7 @@ export const kovan: IContractAddresses = {
 					code: 'LETH',
 					address: '0x977feC72C295EdAf7E7deA5681E42CaFf69749e3'
 				}
-			},
-			[CST.TENOR_M19]: {
-				custodian: {
-					code: 'MOZART-M19',
-					address: '0x56e2727e56F9D6717e462418f822a8FE08Be4711'
-				},
-				aToken: {
-					code: 'sETH-M19',
-					address: '0x2B675f1A282954Ce4FEeb93b9504a6f78B616DE9'
-				},
-				bToken: {
-					code: 'LETH-M19',
-					address: '0x47aC357b44d1b3A0799D91741fA1bD717C1f7900'
-				}
-			},
+			}
 		},
 		[CST.VIVALDI]: {
 			'100C-3H': {
@@ -75,34 +47,6 @@ export const kovan: IContractAddresses = {
 				bToken: {
 					code: 'ETH-100P-3H',
 					address: '0x9f49bb024EB176d227d4b85A58730a5dDeEF529D'
-				}
-			},
-			'105C-3H': {
-				custodian: {
-					code: 'VIVALDI-105C-3H',
-					address: '0x0'
-				},
-				aToken: {
-					code: 'ETH-105C-3H',
-					address: '0x0'
-				},
-				bToken: {
-					code: 'ETH-105P-3H',
-					address: '0x0'
-				}
-			},
-			'095P-3H': {
-				custodian: {
-					code: 'VIVALDI-095P-3H',
-					address: '0x0'
-				},
-				aToken: {
-					code: 'ETH-095P-3H',
-					address: '0x0'
-				},
-				bToken: {
-					code: 'ETH-095C-3H',
-					address: '0x0'
 				}
 			}
 		}
@@ -137,21 +81,7 @@ export const mainnet: IContractAddresses = {
 					code: 'bETH',
 					address: '0xD3E376FdD6071071A9209dc98406297c3e9E7336'
 				}
-			},
-			[CST.TENOR_M19]: {
-				custodian: {
-					code: 'BEETHOVEN-M19',
-					address: ''
-				},
-				aToken: {
-					code: 'aETH-M19',
-					address: ''
-				},
-				bToken: {
-					code: 'bETH-M19',
-					address: ''
-				}
-			},
+			}
 		},
 		[CST.MOZART]: {
 			[CST.TENOR_PPT]: {
@@ -167,66 +97,9 @@ export const mainnet: IContractAddresses = {
 					code: 'LETH',
 					address: '0x96460126E40167a145A41978E261F34E4f2C4706'
 				}
-			},
-			[CST.TENOR_M19]: {
-				custodian: {
-					code: 'MOZART-M19',
-					address: ''
-				},
-				aToken: {
-					code: 'sETH-M19',
-					address: ''
-				},
-				bToken: {
-					code: 'LETH-M19',
-					address: ''
-				}
-			},
-		},
-		[CST.VIVALDI]: {
-			'100C-3H': {
-				custodian: {
-					code: 'VIVALDI-100C-3H',
-					address: '0xBEFf9eEE8E1055A5ACd7a690Fe5B295a346D7346'
-				},
-				aToken: {
-					code: 'ETH-100C-3H',
-					address: '0xaaa743285bcAB4F015890f67354204fF2228fc3f'
-				},
-				bToken: {
-					code: 'ETH-100P-3H',
-					address: '0x5EEfab02C9A7059Ba463c41BeC2233A9Fac5f0F1'
-				}
-			},
-			'105C-3H': {
-				custodian: {
-					code: 'VIVALDI-105C-3H',
-					address: '0x0'
-				},
-				aToken: {
-					code: 'ETH-105C-3H',
-					address: '0x0'
-				},
-				bToken: {
-					code: 'ETH-105P-3H',
-					address: '0x0'
-				}
-			},
-			'095P-3H': {
-				custodian: {
-					code: 'VIVALDI-095P-3H',
-					address: '0x0'
-				},
-				aToken: {
-					code: 'ETH-095P-3H',
-					address: '0x0'
-				},
-				bToken: {
-					code: 'ETH-095C-3H',
-					address: '0x0'
-				}
 			}
-		}
+		},
+		[CST.VIVALDI]: {}
 	},
 	Oracles: [
 		{
